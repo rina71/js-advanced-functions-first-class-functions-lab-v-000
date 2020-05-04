a699bc70d23bd8797ee70f07360ff67c0168ea19
Yurina Shin <yurinas@gmail.com>
2020-05-04 04:21:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+  const myFunc = function() {
+      return [drivers[0], drivers[1]];
+  };
+return myFunc();
+}
+
+function returnLastTwoDrivers(drivers){
+  const myFunc = function() {
+      return drivers.slice(-2);
+  };
+return myFunc();
+}
+
+function selectingDrivers(){
+  const array = [returnFirstTwoDrivers(),returnLastTwoDrivers() ]
+  }
+  return array;
+}
